1aeddc871e3728f07c1798ad9650626261b07906
Adam McKenna <adam-mckenna@outlook.com>
2018-01-18 20:34:44 +0000

    JWT auth token and User Transformer

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\UserTransformer;
 use Dingo\Api\Contract\Http\Request;
+use Dingo\Api\Routing\Helpers;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
 class AuthenticationController extends Controller
 {
+    use Helpers;
+
     public function authenticate(Request $request)
     {
         // grab credentials from the request
         $credentials = $request->only('email', 'password');
-        dd($credentials);
         try {
             // attempt to verify the credentials and create a token for the user
             if (!$token = JWTAuth::attempt($credentials)) {
@@ -22,8 +25,10 @@ class AuthenticationController extends Controller
             // something went wrong whilst attempting to encode the token
             return response()->json(['error' => 'could_not_create_token'], 500);
         }
-
         // all good so return the token
-        return response()->json(compact('token'));
+//        return ;
+
+        return $this->response->item(auth()->user(), new UserTransformer)
+            ->addMeta('token', JWTAuth::fromUser(auth()->user()));
     }
 }
---
+++ app/UserTransformer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    public function transform(User $user)
+    {
+        return [
+            'id'        => $user->id,
+            'name'      => $user->name,
+            'email'     => $user->email,
+            'password'  => $user->password,
+        ];
+    }
+}
